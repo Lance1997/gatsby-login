991c32563edc035bb9c5f2930f7088511226aba4
Lance <lanarmah@gmail.com>
2020-07-04 17:47:24 +0000

    added signin/signup

--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,11 +1,46 @@
 import React from "react"
-import Header from "../components/header"
+import Layout from "../components/layout"
+import Button from '../components/button'
+import Input from "../components/input"
 import classes from './signin.module.css'
 
 export default function Signin() {
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    let email = document.getElementById('email-label').value;
+    alert ("Thank you for signing in. Enjoy! "+ email);
+    if (typeof window !== `undefined`) window.location.replace(`/`)
+  }
+
   return (
-    <div className={classes.Signin}>
-      <Header title="SIGN IN"/>
-    </div>
+    <Layout>
+      <div id="signin">
+
+        <h1 className={classes.Signin}>SIGN IN</h1>
+
+        <form onSubmit={formSubmitHandler} className="form" method="POST">
+          <div className="form-group">
+            <label htmlFor="email-label">
+              <span>Email</span> <br />
+              <Input id="email-label" type="email" name="email" mode="email" required={true} placeholder="Enter Email here" />
+            </label>
+          </div>
+          <div className="form-group">
+            <label htmlFor="pass-label">
+              <span>Password</span> 
+              <Button id="toggle-password" style={{marginBottom: `10px`, float: `right`}}  type="button" text="Show password"/>
+              <br />
+              <Input type="password" placeholder="Enter Password here" id="pass-label" name="current-password" complete="current-password" length="8" required={true} ariaDescribedby="password-constraints"/>
+              <br />
+              <div id="password-constraints" style={{color: `yellow`}}>Eight or more characters with a mix <br /> of letters, numbers and symbols.</div>
+            </label>
+          </div>
+          <div>
+            <Button style={{ height: `50px`, fontSize: `1.2rem`, width: `100%`}} type="submit" text="Sign In"/>
+          </div>
+        </form>
+      </div>
+    </Layout>
   );
 }
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,11 +1,65 @@
 import React from "react"
-import Header from "../components/header"
+import Layout from "../components/layout"
+import Button from '../components/button'
+import Input from "../components/input"
+// import { graphql } from 'gatsby'
 import classes from './signup.module.css'
 
-export default function Signup() {
+export default function Signup({data}) {
+
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    let email = document.getElementById('email-label').value;
+    alert ("Thank you for signing up. Enjoy! "+ email);
+    if (typeof window !== `undefined`) window.location.replace(`/`)
+  }
+
   return (
-    <div className={classes.Signup}>
-      <Header title="SIGN UP"/>
-    </div>
+    <Layout>
+      {/* {data.site.siteMetadata.title} */}
+      <div>
+        <h1 className={classes.Signup}>SIGN UP</h1>
+        <form onSubmit={formSubmitHandler} className="form">
+          <div className="form-group">
+            <label htmlFor="new-email-label">
+              <span>Email</span> <br />
+              <Input id="new-email-label" type="email" name="email" mode="email" required={true} placeholder="Enter Email here" />
+            </label>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="new-pass-label">
+              <span>Password</span> 
+              <Button id="toggle-password" style={{marginBottom: `10px`, float: `right`}}  type="button" text="Show password" />
+              <br />
+              <Input type="password" placeholder="Enter Password here" id="new-pass-label" name="new-password" complete="new-password" length="8" required={true} ariaDescribedby="password-constraints" />
+              <br />
+              <div id="password-constraints" style={{color: `yellow`}}>Eight or more characters with a mix <br /> of letters, numbers and symbols.</div>
+            </label>
+          </div>
+
+          <div className="form-group" style={{marginTop: `5px`}}>
+            <label htmlFor="new-pass-confirm-label">
+              <span>Confirm Password</span><br />
+              <Input type="password" placeholder="Confirm Password" id="new-pass-confirm-label" name="new-password-confirmation" length="8" required={true}/>
+              <br />
+            </label>
+          </div>
+          <div>
+            <Button style={{ height: `50px`, fontSize: `1.2rem`, width: `100%`}} type="submit" text="Sign Up"/>
+          </div>
+        </form>
+      </div>
+    </Layout>
   );
 }
+
+// export const query = graphql`
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//   }
+// `
